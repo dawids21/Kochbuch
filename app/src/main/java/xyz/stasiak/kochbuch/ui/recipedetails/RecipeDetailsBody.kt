a30133cc9ee1c1798d9f341a9d5a6f6baf85ab57
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-04-26 14:55:53 +0200

    Extract portion counter to separate composable

--- app/src/main/java/xyz/stasiak/kochbuch/ui/recipedetails/RecipeDetailsBody.kt
+++ app/src/main/java/xyz/stasiak/kochbuch/ui/recipedetails/RecipeDetailsBody.kt
@@ -2,7 +2,6 @@ package xyz.stasiak.kochbuch.ui.recipedetails
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,13 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -64,31 +58,17 @@ fun RecipeDetailsBody(
             style = MaterialTheme.typography.h5
         )
         var numberOfPortions by remember { mutableStateOf(1) }
-        Text(
-            text = stringResource(R.string.portions, numberOfPortions),
-            modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 16.dp)
-                .fillMaxWidth(),
-            style = MaterialTheme.typography.h6
-        )
-        Row(){
-            IconButton(
-                onClick = {
-                    if (numberOfPortions > 1)
-                        numberOfPortions--
-                }
-            ) {
-                Icon(Icons.Default.Remove, null)
-            }
-            IconButton(
-                onClick = {
-                    if (numberOfPortions < 10)
+        PortionCounter(
+            numberOfPortions = numberOfPortions,
+            onPlusClicked = {
+                if (numberOfPortions > 1)
+                    numberOfPortions--
+            },
+            onMinusClicked = {
+                if (numberOfPortions < 10)
                     numberOfPortions++
-                }
-            ) {
-                Icon(Icons.Default.Add, null)
             }
-        }
+        )
         Text(
             text = stringResource(R.string.ingredients),
             modifier = Modifier
