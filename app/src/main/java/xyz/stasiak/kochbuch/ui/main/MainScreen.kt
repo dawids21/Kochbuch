37976417398496f195646efd6d7ec16d7b9e5cff
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-05-03 10:20:46 +0200

    feat: Add swipe navigation

--- app/src/main/java/xyz/stasiak/kochbuch/ui/main/MainScreen.kt
+++ app/src/main/java/xyz/stasiak/kochbuch/ui/main/MainScreen.kt
@@ -1,15 +1,25 @@
 package xyz.stasiak.kochbuch.ui.main
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import xyz.stasiak.kochbuch.ui.AppViewModelProvider
 import xyz.stasiak.kochbuch.ui.main.info.BottomInfoDestination
 import xyz.stasiak.kochbuch.ui.main.info.InfoScreen
@@ -17,6 +27,7 @@ import xyz.stasiak.kochbuch.ui.main.recipes.BottomMainCourseDestination
 import xyz.stasiak.kochbuch.ui.main.recipes.BottomSoupDestination
 import xyz.stasiak.kochbuch.ui.main.recipes.RecipeScreen
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun MainScreen(
     navController: NavHostController,
@@ -26,10 +37,44 @@ fun MainScreen(
 ) {
     val mainCourses by viewModel.mainCourses.collectAsState()
     val soups by viewModel.soups.collectAsState()
-    // TODO dodać gest przeciągnięcia do przechodzenia między kartami
+
+    val swipeableState = rememberSwipeableState(initialValue = 0)
+    val sizePx = with(LocalDensity.current) { 200.dp.toPx() }
+    val anchors = mapOf(-sizePx to 1, 0f to 0, sizePx to -1)
+    val destinations = listOf(
+        BottomInfoDestination, BottomSoupDestination, BottomMainCourseDestination
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    LaunchedEffect(swipeableState.currentValue) {
+        if (swipeableState.currentValue == 0) {
+            return@LaunchedEffect
+        }
+        destinations.find { it.route == currentRoute }?.let { destination ->
+            val index = destinations.indexOf(destination)
+            val nextIndex = index + swipeableState.currentValue
+            if (nextIndex in destinations.indices) {
+                navController.navigate(destinations[nextIndex].route) {
+                    navController.graph.startDestinationRoute?.let { screenRoute ->
+                        popUpTo(screenRoute) {
+                            saveState = true
+                        }
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+            swipeableState.snapTo(0)
+        }
+    }
     Scaffold(
         bottomBar = { MainBottomBar(navController = navController) },
-        modifier = modifier
+        modifier = modifier.swipeable(
+            state = swipeableState,
+            anchors = anchors,
+            thresholds = { _, _ -> FractionalThreshold(0.5f) },
+            orientation = Orientation.Horizontal
+        )
     ) { innerPadding ->
         NavHost(
             navController = navController,
