4deab920da8ed01503367277150c6d2695aeab6d
Milosz Matuszewski <myyylek@gmail.com>
2023-05-08 22:21:15 +0200

    basic info screen

--- app/src/main/java/xyz/stasiak/kochbuch/ui/main/info/InfoScreen.kt
+++ app/src/main/java/xyz/stasiak/kochbuch/ui/main/info/InfoScreen.kt
@@ -14,12 +14,21 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun InfoScreen(modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()
     ) {
-        Text(text = "Info")
-        Spacer(modifier = Modifier.height(64.dp))
+        Column() {
+            Text(text = "Witamy w  Kochbuch!")
+            Text(text = "Jedyna aplikacja, która pozwoli ci zostać mistrzem kuchni!")
+            Text(text = "Funkcje:")
+            Text(text = "1. Przepisy na najlepsze zupy świata")
+            Text(text = "2. Przepisy na najlepsze dania główne świata")
+            Text(text = "3. Lista zakupów")
+            Text(text = "4. Lista kroków jak przygotować danie")
+            Text(text = "5. Minutnik")
+        }
+        Spacer(modifier = Modifier.height(4.dp))
         InfoAnimation(300.dp)
     }
 }
